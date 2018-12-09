005acd161140d1f5813f97df772657edff1331bd
Gabriel <pgabr001@fiu.edu>
2018-12-09 03:12:53 -0500

    Update to Notepad_Injector
    Add the ability to present the Notepad PID before requesting it.

--- Notepad_Injector/Notepad_Injector.cpp
+++ Notepad_Injector/Notepad_Injector.cpp
@@ -4,12 +4,38 @@
 #include <cstring>
 #include <Windows.h>
 #include <easyhook.h>
+#include <Psapi.h>
+#include <algorithm>
+
+using namespace std;
+
+wstring StringToWString(const string& s);
+
+wstring StringToWString(const string& s)
+{
+	std::wstring temp(s.length(), L' ');
+	std::copy(s.begin(), s.end(), temp.begin());
+	return temp;
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	string s1 = "Untitled - Notepad";
+	wstring s2 = StringToWString(s1);
+
+	HWND windowHandle = FindWindowW(NULL, s2.c_str());
+	DWORD* processID = new DWORD;
+
+	GetWindowThreadProcessId(windowHandle, processID);
+
+	wcout << "Grabbed Notepad PID: \n";
+	wcout << *processID << endl;
 	DWORD processId;
 	std::wcout << "Enter the target process Id: ";
 	std::cin >> processId;
+
+
 
 	WCHAR* dllToInject = L"..\\x64\\Debug\\Notepad_Hook.dll";
 	wprintf(L"Attempting to inject: %s\n\n", dllToInject);
